920b18c0d459911ef8428c620714e1e8753ee058
Ksh Siaan <kshsiaan@gmail.com>
2025-04-23 14:08:16 +0600

    landing

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -7,28 +7,30 @@ import BrandRegister from "./brand-reg";
 import WholeRegister from "./whole-reg";
 import AssosRegister from "./assos-reg";
 
-export default function Page() {
+function RegistrationContent() {
   const searchParams = useSearchParams();
   const as = searchParams.get("as");
 
+  switch (as) {
+    case "member":
+      return <MemberRegister />;
+    case "store":
+      return <StoreRegister />;
+    case "brand":
+      return <BrandRegister />;
+    case "wholesaler":
+      return <WholeRegister />;
+    case "association":
+      return <AssosRegister />;
+    default:
+      return <MemberRegister />;
+  }
+}
+
+export default function Page() {
   return (
     <Suspense fallback={<div>Loading registration form...</div>}>
-      {(() => {
-        switch (as) {
-          case "member":
-            return <MemberRegister />;
-          case "store":
-            return <StoreRegister />;
-          case "brand":
-            return <BrandRegister />;
-          case "wholesaler":
-            return <WholeRegister />;
-          case "association":
-            return <AssosRegister />;
-          default:
-            return <MemberRegister />;
-        }
-      })()}
+      <RegistrationContent />
     </Suspense>
   );
 }
